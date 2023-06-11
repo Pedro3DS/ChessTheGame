b95495a169f48ef8570b3cc089fe9d92db9f19d3
Javador48 <paaott08@gmail.com>
2023-06-11 18:57:12 -0300

    feature - 01
    feito os def das peças

---
+++ peças.py
@@ -0,0 +1,111 @@
+# --------- PEÇAS PRETAS ----------
+
+# =========-TORRES P-===========
+def torre_P1():
+    print()
+
+def torre_P2():
+    print()
+# =============================
+# =========-CAVALOS P-===========
+def cavalo_P1():
+    print()
+
+def cavalo_P2():
+    print()
+# =============================
+# =========-BISPOS P-===========
+def bispo_P1():
+    print()
+
+def bispo_P2():
+    print()
+# =============================
+# =======-REI E RAINHA P-========
+def rei_P():
+    print()
+
+def rainha_P():
+    print()
+# =============================
+# ===========-PEÕS P-============
+def peao_P1():
+    print()
+
+def peao_P2():
+    print()
+
+def peao_P3():
+    print()
+
+def peao_P4():
+    print()
+
+def peao_P5():
+    print()
+
+def peao_P6():
+    print()
+
+def peao_P7():
+    print()
+
+def peao_P8():
+    print()
+# =============================
+
+# ---------- PEÇAS BRANCAS -----------
+
+# =========-TORRES B-===========
+def torre_B1():
+    print()
+
+def torre_B2():
+    print()
+# =============================
+# =========-CAVALOS B-===========
+def cavalo_B1():
+    print()
+
+def cavalo_B2():
+    print()
+# =============================
+# =========-BISPOS B-===========
+def bispo_B1():
+    print()
+
+def bispo_B2():
+    print()
+# =============================
+# =======-REI E RAINHA B-========
+def rei_B():
+    print()
+
+def rainha_B():
+    print()
+# =============================
+# ===========-PEÕS B-============
+def peao_B1():
+    print()
+
+def peao_B2():
+    print()
+
+def peao_B3():
+    print()
+
+def peao_B4():
+    print()
+
+def peao_B5():
+    print()
+
+def peao_B6():
+    print()
+
+def peao_B7():
+    print()
+    
+def peao_B8():
+    print()
+# =============================
