89ca387cc88b400690eb4a3b791ed7154a98368e
Javador48 <paaott08@gmail.com>
2023-06-20 12:02:58 -0300

    Inversão das peças brancas com as das pretas

--- peças.py
+++ peças.py
@@ -2,68 +2,68 @@
 
 # =========-TORRES P-===========
 def torre_P1():
-    print("♜",end=" ")
+    print("♖",end=" ")
 
 
 def torre_P2():
-    print("♜")
+    print("♖")
 
 # =============================
 # =========-CAVALOS P-===========
 def cavalo_P1():
-    print("♞",end=" " )
+    print("♘",end=" " )
     
 def cavalo_P2():
-    print("♞",end=" " )
+    print("♘",end=" " )
   
 # =============================
 # =========-BISPOS P-===========
 def bispo_P1():
-    print("♝",end=" " )
+    print("♗",end=" " )
    
 def bispo_P2():
-    print("♝",end=" " )
+    print("♗",end=" " )
 
 # =============================
 # =======-REI E RAINHA P-========
 def rei_P():
-     print("♚",end=" " )   
+     print("♔",end=" " ) 
 
 def rainha_P():
-    print("♛",end=" ")
+    print("♕",end=" ")
 
 # =============================
 # ===========-PEÕS P-============
 def peao_P1():
-    print("♟",end=" ")
+    print("♙",end=" ")
 
  
 def peao_P2():
-    print("♟",end=" ")
+    print("♙",end=" ")
 
 
 def peao_P3():
-     print("♟",end=" ")
+     print("♙",end=" ")
 
 
 def peao_P4():
-    print("♟",end=" ")
+    print("♙",end=" ")
 
 
 def peao_P5():
-     print("♟",end=" ")
+     print("♙",end=" ")
 
 
 def peao_P6():
-    print("♟",end=" ")
+    print("♙",end=" ")
 
 
 def peao_P7():
-    print("♟",end=" ")
+    print("♙",end=" ")
 
 
 def peao_P8():
-    print("♟")    
+    print("♙")    
  
 # =============================
 
@@ -71,54 +71,54 @@ def peao_P8():
 
 # =========-TORRES B-===========
 def torre_B1():
-    print("♖",end=" ")
+    print("♜",end=" ")
 
 def torre_B2():
-    print("♖")
+    print("♜")
 # =============================
 # =========-CAVALOS B-===========
 def cavalo_B1():
-    print("♘",end=" ")
+    print("♞",end=" ")
 
 def cavalo_B2():
-    print("♘",end=" ")
+    print("♞",end=" ")
 # =============================
 # =========-BISPOS B-===========
 def bispo_B1():
-    print("♗",end=" ")
+    print("♝",end=" ")
 
 def bispo_B2():
-    print("♗",end=" ")
+    print("♝",end=" ")
 # =============================
 # =======-REI E RAINHA B-========
 def rei_B():
-    print("♔",end=" ")
+    print("♚",end=" ")
 
 def rainha_B():
-    print("♕",end=" ")
+    print("♛",end=" ")
 # =============================
 # ===========-PEÕS B-============
 def peao_B1():
-    print("♙",end=" ")
+    print("♟",end=" ")
 
 def peao_B2():
-    print("♙",end=" ")
+    print("♟",end=" ")
 
 def peao_B3():
-    print("♙",end=" ")
+    print("♟",end=" ")
 
 def peao_B4():
-    print("♙",end=" ")
+    print("♟",end=" ")
 
 def peao_B5():
-    print("♙",end=" ")
+    print("♟",end=" ")
 
 def peao_B6():
-    print("♙",end=" ")
+    print("♟",end=" ")
 
 def peao_B7():
-    print("♙",end=" ")
+    print("♟",end=" ")
     
 def peao_B8():
-    print("♙")
+    print("♟")
 # =============================
